5c0f6008eecd44b88d09d807607116ab3e41bdfa
Emre Hamurcu <emrehamurcu_1995@hotmail.com>
2019-10-11 11:41:22 +0300

    Added -Fast Scroll -Layout animation

--- app/src/main/java/com/emrhmrc/genericrecyclerview/MainActivity.java
+++ app/src/main/java/com/emrhmrc/genericrecyclerview/MainActivity.java
@@ -2,7 +2,13 @@ package com.emrhmrc.genericrecyclerview;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.RelativeLayout;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
@@ -19,7 +25,7 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity implements IOnItemClickListener, IOnSwipe {
+public class MainActivity extends AppCompatActivity implements IOnItemClickListener, IOnSwipe, AdapterView.OnItemSelectedListener {
 
     private static final String TAG = "MainActivity";
     @BindView(R.id.generic_recylerview)
@@ -28,7 +34,12 @@ public class MainActivity extends AppCompatActivity implements IOnItemClickListe
     RelativeLayout emptyView;
     @BindView(R.id.searchView)
     SearchView searchView;
+    @BindView(R.id.spinner)
+    Spinner spinner;
+
+    //Adapter
     TestAdapter testAdapter;
+    //Our Model
     List<TestModel> testModelList;
 
 
@@ -40,6 +51,20 @@ public class MainActivity extends AppCompatActivity implements IOnItemClickListe
         setAdapter();
         prepareData();
         setSearchView();
+        setSpinner();
+    }
+
+    private void setSpinner() {
+        spinner.setOnItemSelectedListener(this);
+        List<String> categories = new ArrayList<>();
+        categories.add("Fall Down");
+        categories.add("From Bottom");
+        categories.add("From Right");
+
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, categories);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
     }
 
     private void setSearchView() {
@@ -63,7 +88,7 @@ public class MainActivity extends AppCompatActivity implements IOnItemClickListe
     private void prepareData() {
         testModelList = new ArrayList<>();
 
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 100; i++) {
             TestModel testModel = new TestModel();
             testModel.setCount(i + 1);
             testModel.setHeader("Header-" + i + 1);
@@ -88,5 +113,33 @@ public class MainActivity extends AppCompatActivity implements IOnItemClickListe
     @Override
     public void OnSwipe(Object item, int position, int direction) {
         Log.d(TAG, "OnSwipe:Position:" + position + " Direction:" + direction);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        int resId = R.anim.layout_animation_fall_down;
+        switch (i) {
+            case 0:
+                resId = R.anim.layout_animation_fall_down;
+                break;
+            case 1:
+                resId = R.anim.layout_animation_from_bottom;
+                break;
+            case 2:
+                resId = R.anim.layout_animation_from_right;
+                break;
+
+        }
+        LayoutAnimationController animation = AnimationUtils.loadLayoutAnimation(this, resId);
+        recyclerView.setLayoutAnimation(animation);
+        testAdapter.getItems().clear();
+        setAdapter();
+        prepareData();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
