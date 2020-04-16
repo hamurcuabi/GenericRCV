d6b46035bab686b9038c8f8824ade5b3239b06fd
Emre Hamurcu <emrehamurcu_1995@hotmail.com>
2020-04-16 18:55:23 +0300

    OnItemClick artık cast edilmeden kullanıbilir

--- genericrecycler/src/main/java/com/emrhmrc/genericrecycler/interfaces/IOnItemClickListener.java
+++ genericrecycler/src/main/java/com/emrhmrc/genericrecycler/interfaces/IOnItemClickListener.java
@@ -1,6 +1,8 @@
 package com.emrhmrc.genericrecycler.interfaces;
 
-public interface IOnItemClickListener<T> extends IGenericBaseRecyclerListener {
+import com.emrhmrc.genericrecycler.models.BaseModel;
+
+public interface IOnItemClickListener<T extends BaseModel> extends IGenericBaseRecyclerListener {
 
     void onItemClicked(T item, int positon);
 }
